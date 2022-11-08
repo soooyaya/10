c4b2c4bd67a4d9d628e6947f1775bfc4d75c783c
soooyaya <hsoo0531@naver.com>
2022-11-08 17:12:35 +0900

    06

--- main.c
+++ main.c
@@ -5,24 +5,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	FILE* fp;
+	FILE* fp = NULL;
+	char c;
 	char str[30];
 	
-	fp = fopen("sample.txt", "w");
-	
-	printf("input a word: ");
-	scanf("%s", str);
-	fprintf(fp, "%s\n", str);
+	fp = fopen("sample.txt", "r");
 
-    printf("input a word: ");
-	scanf("%s", str);
-	fprintf(fp, "%s\n", str);
-	
-	printf("input a word: ");
-	scanf("%s", str);
-	fprintf(fp, "%s\n", str);
-	
-	
+	while ( fgets(str, 30, fp) != NULL)
+	    printf("%s", str);
+	    
+	    
 	fclose(fp);
+	
 	return 0;
 }
